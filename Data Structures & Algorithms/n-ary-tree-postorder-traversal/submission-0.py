64551ab176e880106a8b84fcf7d69450bb7cfd7a
Miguel Dias Henz <163352966+HenzMiguel@users.noreply.github.com>
2026-07-08 22:05:51 -0300

    Add: n-ary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/n-ary-tree-postorder-traversal/submission-0.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        
+        res = []
+        def recurse(node, res):
+            if not node:
+                return None
+            
+            for child in node.children:
+                recurse(child, res)
+
+            res.append(node.val)
+
+        recurse(root, res)
+        return res
